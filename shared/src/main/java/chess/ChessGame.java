b5793b9090354001a590ea2065b78d3bdc1a8f3c
Tate McCauley <tatemccauley@gmail.com>
2024-10-03 15:34:03 -0600

    add resetBoard call to chessgame, fixed all but one test...

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -16,7 +16,7 @@ public class ChessGame {
 
     public ChessGame() {
         this.board = new ChessBoard();
-     //   this.board.resetBoard();
+        this.board.resetBoard();
         this.gameState = new GameState();
         this.teamTurn = TeamColor.WHITE;
     }
