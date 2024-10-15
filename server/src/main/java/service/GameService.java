7e0bca5665213061ab80efb7994f2bec56c5081a
Tate McCauley <tatemccauley@gmail.com>
2024-10-14 21:41:44 -0600

    GameService start

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,20 @@
 package service;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.GameData;
+import model.AuthData;
+
+import java.util.Collection;
+
 public class GameService {
+    private final DataAccess dataAccess;
+    private final AuthService authService;
+
+    public GameService(DataAccess dataAccess, AuthService authService){
+        this.dataAccess = dataAccess;
+        this.authService = authService;
+    }
+
+
 }
