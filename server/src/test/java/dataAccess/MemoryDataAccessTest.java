51e4246efee2ccbbaa723f95466cc3bc703b1de4
Tate McCauley <tatemccauley@gmail.com>
2024-10-18 11:41:18 -0600

    created a test file for MemoryDataAccess

---
+++ server/src/test/java/dataAccess/MemoryDataAccessTest.java
@@ -0,0 +1,52 @@
+package dataAccess;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryDataAccessTest {
+
+    @Test
+    void generateGameId() {
+    }
+
+    @Test
+    void clearAll() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void createGame() {
+    }
+
+    @Test
+    void getGame() {
+    }
+
+    @Test
+    void updateGame() {
+    }
+
+    @Test
+    void listGames() {
+    }
+
+    @Test
+    void createAuth() {
+    }
+
+    @Test
+    void getAuth() {
+    }
+
+    @Test
+    void deleteAuth() {
+    }
+}
