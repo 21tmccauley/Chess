aa118606b41872d0064379e4e73f0966c8780837
Tate McCauley <tatemccauley@gmail.com>
2024-10-18 11:29:38 -0600

    created tests for equal, hashcode, and tostring  for AuthData model

--- shared/src/test/java/model/AuthDataTest.java
+++ shared/src/test/java/model/AuthDataTest.java
@@ -16,4 +16,30 @@ class AuthDataTest {
         assertEquals(authToken, authData.authToken());
         assertEquals(userName, authData.username());
     }
+    @Test
+    public void testEquality() {
+        AuthData auth1 = new AuthData("token123", "user1");
+        AuthData auth2 = new AuthData("token123", "user1");
+        AuthData auth3 = new AuthData("differentToken", "user1");
+        AuthData auth4 = new AuthData("token123", "differentUser");
+
+        assertEquals(auth1, auth2);
+        assertNotEquals(auth1, auth3);
+        assertNotEquals(auth1, auth4);
+    }
+
+    @Test
+    public void testHashCode() {
+        AuthData auth1 = new AuthData("token123", "user1");
+        AuthData auth2 = new AuthData("token123", "user1");
+
+        assertEquals(auth1.hashCode(), auth2.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        AuthData authData = new AuthData("token123", "user1");
+        String expected = "AuthData[authToken=token123, username=user1]";
+        assertEquals(expected, authData.toString());
+    }
 }
