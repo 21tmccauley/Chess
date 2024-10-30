e2a048bbc0f9584eb2701367ede68cd82733955d
Tate McCauley <tatemccauley@gmail.com>
2024-10-30 12:16:09 -0600

    Code quality change - removed comment block

--- shared/src/main/java/chess/FixedMoveStrategy.java
+++ shared/src/main/java/chess/FixedMoveStrategy.java
@@ -36,32 +36,4 @@ public class FixedMoveStrategy implements MoveStrategy {
 
         return moves;
     }
-
-//    private void addCastlingMoves(ChessPiece piece, ChessBoard board, ChessPosition position, GameState gameState, Collection<ChessMove> moves) {
-//        ChessGame.TeamColor color = piece.getTeamColor();
-//        int row = color == ChessGame.TeamColor.WHITE ? 1 : 8;
-//
-//        if (gameState.canCastle(color, true)) {
-//            // Check kingside castling
-//            if (isCastlingPathClear(board, row, 5, 7)) {
-//                moves.add(new ChessMove(position, new ChessPosition(row, 7), null));
-//            }
-//        }
-//
-//        if (gameState.canCastle(color, false)) {
-//            // Check queenside castling
-//            if (isCastlingPathClear(board, row, 3, 4)) {
-//                moves.add(new ChessMove(position, new ChessPosition(row, 3), null));
-//            }
-//        }
-//    }
-
-    private boolean isCastlingPathClear(ChessBoard board, int row, int startCol, int endCol) {
-        for (int col = startCol; col <= endCol; col++) {
-            if (board.getPiece(new ChessPosition(row, col)) != null) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
