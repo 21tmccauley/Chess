e6b5783c6371694de9d516972a8bc37298ed4326
Tate McCauley <tatemccauley@gmail.com>
2024-11-12 19:16:25 -0700

    Added escape sequences

--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -61,5 +61,9 @@ public class EscapeSequences {
     public static final String BLACK_PAWN = " ♟ ";
     public static final String EMPTY = " \u2003 ";
 
+    public static final String RESET_ALL = UNICODE_ESCAPE + "[0m";
+    public static final String SET_BG_LIGHT = SET_BG_COLOR + "252m";  // light grey
+    public static final String SET_BG_DARK = SET_BG_COLOR + "240m";   // dark grey
+
     public static String moveCursorToLocation(int x, int y) { return UNICODE_ESCAPE + "[" + y + ";" + x + "H"; }
 }
